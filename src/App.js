56259cda5a0c5f50ed6e033a543890c4a48210fa
Vennesavee <126670814+Vennesavee@users.noreply.github.com>
2024-06-07 18:37:42 +0300

    Imported Axios to display the temperature of a city

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
+import axios from 'axios';
+import ReactAnimatedWeather from 'react-animated-weather';
 import './App.css';
 
 function App() {
+  let apiKey = "3f6be1c407b0d9d1933561808db358ba";
+  let city = "Paris";
+  let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+
+  function showTemperature(response){
+    alert (response.data.main.temp);
+
+
+  }
+  axios.get(url).then(showTemperature);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Hello Vee</h1>
+        <p> I love You </p>
+        <h2>wakonyo</h2>
+        <ReactAnimatedWeather
+    icon="CLEAR_DAY"
+    color="goldenrod"
+    size={512}
+    animate= {true}
+  />
+     
       </header>
     </div>
   );
